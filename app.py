487c76deeee255d91059e763b5f38842e1516502
KTM28 <56693434+KTM28@users.noreply.github.com>
2020-06-22 14:23:13 +0100

    Implement basic views and routes

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-#app.config["TEMPLATES_AUTO_RELOAD"]=True 
+app.config["TEMPLATES_AUTO_RELOAD"]=True 
 
 @app.route('/')
 def index():
-    return f'<h1>ello Flask</h1>'
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
+
+@app.route('/<username>')
+def user(username):
+    return f"Hi {username}"
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return f"{username}: {message}"
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
